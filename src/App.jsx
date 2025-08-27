85c7f3fea3bf4d33f8f90d298e18075bc1039aec
sujalbarnwal9603 <sujalbarnwal9603@gmail.com>
2025-08-27 12:34:13 +0530

    portfolio setup

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from "./sections/Navbar.jsx"
+const App=()=> {
+  return (
+    <div className='container mx-auto max-w-7xl'>
+      <Navbar/>
+      {/* hero */}
+      {/* about */}
+      {/* experience */}
+      {/* projects */}
+      {/* contact me */}
+      {/* footer */}
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/sections/Navbar.jsx
@@ -0,0 +1,19 @@
+
+const Navbar=()=> {
+  return (
+    <div className="fixed inset-x-0 z-20 w-full backdrop-blur-lg bg-primary/40">
+      <div className="mx-auto c-space max-w-7xl">
+        <div className="flex items-center justify-between py-2 sm:py-0">
+            <a href='/' className="text-xl font-bold transition-colors text-neutral-400 hover:text-white">
+                SUJAL
+            </a>
+            <button>
+                <img src="" alt="" />
+            </button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
