a0a2237aa1dd5c1bce083bbaf90c2de6889cb2c6
sujalbarnwal9603 <sujalbarnwal9603@gmail.com>
2025-08-28 19:40:27 +0530

    background

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,9 +1,10 @@
 import HeroText from "../components/HeroText.jsx"
-
+import ParallaxBackground from "../components/ParallaxBackground.jsx"
 const Hero = () => {
   return (
     <section className="flex items-start justify-center min-h-screen overflow-hidden-space md:items-start md:justify-start">
       <HeroText/>
+      <ParallaxBackground/>
     </section>
   )
 }
