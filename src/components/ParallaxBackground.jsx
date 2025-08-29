06f12507672ba17084ce3120a4fd73548015f0e0
sujalbarnwal9603 <sujalbarnwal9603@gmail.com>
2025-08-29 22:18:04 +0530

    hero

--- src/components/ParallaxBackground.jsx
+++ src/components/ParallaxBackground.jsx
@@ -1,11 +1,12 @@
-import {motion, useScroll, useTransform} from "motion/react"
+import {motion, useScroll, useSpring, useTransform} from "motion/react"
 
 const ParallaxBackground = () => {
     const {scrollYProgress} = useScroll();
-    const mountain3Y=useTransform(scrollYProgress, [0,0.5],["0%", "70%"])
-    const planetsX=useTransform(scrollYProgress, [0,0.5],["0%", "-20%"])
-    const mountain2Y=useTransform(scrollYProgress, [0,0.5],["0%", "30%"])
-    const mountain1Y=useTransform(scrollYProgress, [0,0.5],["0%", "0%"])
+    const x = useSpring(scrollYProgress,{damping:50})
+    const mountain3Y=useTransform(x, [0,0.5],["0%", "70%"])
+    const planetsX=useTransform(x, [0,0.5],["0%", "-20%"])
+    const mountain2Y=useTransform(x, [0,0.5],["0%", "30%"])
+    const mountain1Y=useTransform(x, [0,0.5],["0%", "0%"])
 
   return (
     <section className='absolute inset-0 bg-black/40'>
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,10 +1,21 @@
+import { Canvas } from "@react-three/fiber"
 import HeroText from "../components/HeroText.jsx"
 import ParallaxBackground from "../components/ParallaxBackground.jsx"
+import  Astronaut  from "../components/Astronaut.jsx"
+import { OrbitControls } from "@react-three/drei"
+
 const Hero = () => {
   return (
     <section className="flex items-start justify-center min-h-screen overflow-hidden-space md:items-start md:justify-start">
       <HeroText/>
       <ParallaxBackground/>
+      <figure className="absolute inset-0"
+      style={{width:"100vw", height:"100vh"}}>
+        <Canvas>
+          <Astronaut/>
+          <OrbitControls/>
+        </Canvas>
+      </figure>
     </section>
   )
 }
