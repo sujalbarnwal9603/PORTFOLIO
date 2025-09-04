0f00681423ee2e1fbd563fa0abf0b7e600793505
sujalbarnwal9603 <sujalbarnwal9603@gmail.com>
2025-09-04 13:43:18 +0530

    images.changed

--- src/constants/index.js
+++ src/constants/index.js
@@ -231,12 +231,13 @@ export const mySocials = [
   },
   {
     name: "Linkedin",
-    href: "https://www.linkedin.com/in/ali-sanati/",
+    href: "https://www.linkedin.com/in/sujal-barnwal-1385aa281/",
     icon: "/assets/socials/linkedIn.svg",
   },
+  
   {
     name: "Instagram",
-    href: "https://www.instagram.com/ali.sanatidev/reels/",
+    href: "https://www.instagram.com/barnwal_p_sujal/",
     icon: "/assets/socials/instagram.svg",
   },
 ];
@@ -276,53 +277,53 @@ export const experiences = [
     ],
   },
 ];
-export const reviews = [
-  {
-    name: "Jack",
-    username: "@jack",
-    body: "I've never seen anything like this before. It's amazing. I love it.",
-    img: "https://robohash.org/jack",
-  },
-  {
-    name: "Jill",
-    username: "@jill",
-    body: "I don't know what to say. I'm speechless. This is amazing.",
-    img: "https://robohash.org/jill",
-  },
-  {
-    name: "John",
-    username: "@john",
-    body: "I'm at a loss for words. This is amazing. I love it.",
-    img: "https://robohash.org/john",
-  },
-  {
-    name: "Alice",
-    username: "@alice",
-    body: "This is hands down the best thing I've experienced. Highly recommend!",
-    img: "https://robohash.org/alice",
-  },
-  {
-    name: "Bob",
-    username: "@bob",
-    body: "Incredible work! The attention to detail is phenomenal.",
-    img: "https://robohash.org/bob",
-  },
-  {
-    name: "Charlie",
-    username: "@charlie",
-    body: "This exceeded all my expectations. Absolutely stunning!",
-    img: "https://robohash.org/charlie",
-  },
-  {
-    name: "Dave",
-    username: "@dave",
-    body: "Simply breathtaking. The best decision I've made in a while.",
-    img: "https://robohash.org/dave",
-  },
-  {
-    name: "Eve",
-    username: "@eve",
-    body: "So glad I found this. It has changed the game for me.",
-    img: "https://robohash.org/eve",
-  },
-];
+// export const reviews = [
+//   {
+//     name: "Jack",
+//     username: "@jack",
+//     body: "I've never seen anything like this before. It's amazing. I love it.",
+//     img: "https://robohash.org/jack",
+//   },
+//   {
+//     name: "Jill",
+//     username: "@jill",
+//     body: "I don't know what to say. I'm speechless. This is amazing.",
+//     img: "https://robohash.org/jill",
+//   },
+//   {
+//     name: "John",
+//     username: "@john",
+//     body: "I'm at a loss for words. This is amazing. I love it.",
+//     img: "https://robohash.org/john",
+//   },
+//   {
+//     name: "Alice",
+//     username: "@alice",
+//     body: "This is hands down the best thing I've experienced. Highly recommend!",
+//     img: "https://robohash.org/alice",
+//   },
+//   {
+//     name: "Bob",
+//     username: "@bob",
+//     body: "Incredible work! The attention to detail is phenomenal.",
+//     img: "https://robohash.org/bob",
+//   },
+//   {
+//     name: "Charlie",
+//     username: "@charlie",
+//     body: "This exceeded all my expectations. Absolutely stunning!",
+//     img: "https://robohash.org/charlie",
+//   },
+//   {
+//     name: "Dave",
+//     username: "@dave",
+//     body: "Simply breathtaking. The best decision I've made in a while.",
+//     img: "https://robohash.org/dave",
+//   },
+//   {
+//     name: "Eve",
+//     username: "@eve",
+//     body: "So glad I found this. It has changed the game for me.",
+//     img: "https://robohash.org/eve",
+//   },
+// ];
