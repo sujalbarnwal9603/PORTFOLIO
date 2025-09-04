7055c806942d4d53f37bce11bcffbf9524cad0df
sujalbarnwal9603 <sujalbarnwal9603@gmail.com>
2025-09-04 09:55:37 +0530

    Add Projects section and project detail components

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from "./sections/Navbar.jsx"
 import Hero from "./sections/Hero.jsx"
 import About from "./sections/About.jsx"
+import Projects from "./sections/Projects.jsx"
 const App=()=> {
   return (
     <div className='container mx-auto max-w-7xl'>
       <Navbar/>
       <Hero/>
       <About/>
+      <Projects/>
       <section className='min-h-screen'></section>
       <section className='min-h-screen'></section>
       <section className='min-h-screen'></section>
